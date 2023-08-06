8e7ade563139027b66c58ab3a93613bd46a0e17b
Diego Medina <xecnon@gmail.com>
2023-08-06 19:08:44 -0300

    blog_list exercises from 4.1 to 4.2

---
+++ parte4/list_blogs/utils/middleware.js
@@ -0,0 +1,38 @@
+const logger = require('./logger')
+
+const requestLogger = (request, response, next) => {
+  logger.info('Method:', request.method)
+  logger.info('Path:  ', request.path)
+  logger.info('Body:  ', request.body)
+  logger.info('---')
+  next()
+}
+
+// este podra ejecutarse solo si nunguna ruta atiende la peticion entrante (unknownEndPoint)
+const unknownEndpoint = (request,response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+
+// creo otro middleware para controlar errores espesificos si lo deseo
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  if (error.name === 'SyntaxError') {
+    return response.status(400).send({ error: 'JSON malformatted' , msg: error.message })
+  }
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error) // lo demas errores pasa el default middleware de errores de express
+}
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler
+}
